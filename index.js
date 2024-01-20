81fe15da9c84123c52712c984a7f9ad19b577e3d
ko <pleiades1424@gmail.com>
2024-01-20 23:33:13 +0900

    commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 "use strict";
 
-function hello(name) {
-  console.log("こんにちは" + name + "さん");
-}
+const obj = {
+  Math: 80,
+  English: 90,
+  Science: 60,
+  Society: 50,
+};
 
-hello("kou");
+Object.keys(obj).forEach((key) => {
+  const value = obj[key];
+  console.log(`${key}は${value}点です。`);
+});
